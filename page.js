3b4d4510a340d48786004a41b420a4f57eb7942d
shahhuzaifa <143290124+shahhuzaifa@users.noreply.github.com>
2023-09-19 16:45:53 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,45 @@
+
+for(i=0; i<10; i++){ 
+    console.log("heloo world" +(i+1))
+
+}
+//answer2 
+for(i=1; i<=12; i++){
+document.write("shah huzaifa"+i ,"<br>")
+}
+//answer 3
+for (var i = 0; i <= 4; i++){
+
+}
+//answer 5
+for(var a=3; a>=0; a--){
+    console.log("shah"+a)
+
+}
+//answer 6
+var number = [1,2,3,4,5,6]
+var shah01= number.length
+console.log(shah01)
+//answer 7
+
+var flag = true; 
+// answer 8
+var pets =["cat ", "dog","fish","pigon"]
+for (var i=0; i<=pets.length; i++){
+    console.log("Pet #" + (i + 1) + ": " + pets[i]);
+}
+//answer 10
+var matchFound = false;
+var userInput = "karachi"; 
+var list = ["lahore", "karachi", "kashmir"]; 
+
+for (var i = 0; i < list.length; i++) {
+  if (userInput === list[i]) {
+    alert("karachi is the safest city  ");
+    matchFound = true;
+    break;
+  }
+}
+
+
+
